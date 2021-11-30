1c9994be6bb6cad1e80e0df022037978841f4058
Kevin <kevinsevinche@gmail.com>
2021-11-30 00:00:24 -0500

    Update seeds filed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Greeting.create(greeting: "Hello world!")
+Greeting.create(greeting: "Good morning")
+Greeting.create(greeting: "Good bye")
+Greeting.create(greeting: "Good evening")
+Greeting.create(greeting: "Good afternoon")
